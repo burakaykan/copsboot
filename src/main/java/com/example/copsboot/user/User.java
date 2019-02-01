51df958c50d31b0e3f8ff9ae1828d7603912a294
Burak Aykan Ürer <burakaykan2@gmail.com>
2019-02-01 15:45:05 +0300

    Created: UserDomain

--- src/main/java/com/example/copsboot/user/User.java
+++ src/main/java/com/example/copsboot/user/User.java
@@ -1,14 +1,25 @@
 package com.example.copsboot.user;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 import java.util.UUID;
 
+@Entity
+@Table(name = "copsboot_user")
 public class User {
 
+    @Id
     private UUID id;
     private String email;
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @NotNull
     private Set<UserRole> roles;
+
+    protected User() { }
 
     public User(UUID id, String email, String password, Set<UserRole> roles) {
         this.id = id;
--- src/main/java/com/example/copsboot/user/UserRole.java
+++ src/main/java/com/example/copsboot/user/UserRole.java
@@ -5,3 +5,7 @@ public enum UserRole {
     CAPTAIN,
     ADMIN
 }
+
+//“Officer is a police officer who does the field work.”
+//“Captain is the boss of the officers.”
+//“Admin is an administrative role that has access to all parts of the application.”
